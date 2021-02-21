a92b093aaf03fee00c4e2d20062f30fbede141b3
walter <wtoriola@calpoly.edu>
2021-02-20 18:30:10 -0800

    -Sorts completed

--- sorts.py
+++ sorts.py
@@ -2,11 +2,40 @@ import random
 import time
 
 def selection_sort(list):
-    pass
-    
+    count = 0
+    # Traverse through all array elements
+    for i in range(len(list)):
+
+        # Find the minimum element in remaining
+        # unsorted array
+        min_idx = i
+        for j in range(i+1, len(list)):
+            if list[min_idx] > list[j]:
+                min_idx = j
+
+        # Swap the found minimum element with
+        # the first element
+        list[i], list[min_idx] = list[min_idx], list[i]
+        count +=1
+        return count
+
 def insertion_sort(list):
-    pass
-   
+    count = 0
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(list)):
+
+        key = list[i]
+
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+        while j >=0 and key < list[j] :
+            list[j+1] = list[j]
+            j -= 1
+        list[j+1] = key
+        count+=1
+    return count
 
 def main():
     # Code coverage NOT required for main
--- sorts_tests.py
+++ sorts_tests.py
@@ -3,11 +3,18 @@ from sorts import *
 
 class TestLab4(unittest.TestCase):
 
-    def test_simple(self):
+    def test_selection_sort(self):
         nums = [23, 10]
         comps = selection_sort(nums)
         self.assertEqual(comps, 1)
         self.assertEqual(nums, [10, 23])
+
+    def test_insertion_sort(self):
+        nums = [23, 10, 15]
+        comps = insertion_sort(nums)
+        self.assertEqual(comps, 2)
+        self.assertEqual(nums, [10, 15, 23])
+
 
 if __name__ == '__main__': 
     unittest.main()
